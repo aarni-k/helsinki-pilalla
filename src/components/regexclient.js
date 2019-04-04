b32f162eecfa5595865d7444856db968424f3cdb
aarni-k <karjalainen.aarni@gmail.com>
2019-04-04 16:30:08 +0300

    added comments to regexclient.js

--- src/components/regexclient.js
+++ src/components/regexclient.js
@@ -1,32 +1,45 @@
-export function regExPortal (data) {
-    // console.log (data, "<- regex function data");
-    var kiitos = /sinu/i
+export function regExPortal(data) {
+    // Search term 
+    var searchTerm = /sinu/i
+    // Finds the next whitespace
     var blanko = /\s{1,1}/
-    var testiA = [
-     {a : "Kiitos kaikille avusta."},
-     {a : "Aina välillä tarvitaan kiitosta"},
-     {a : "Kuka sanoi kiitos sinulle metrossa?"},
-     {a : "Seuraava sana on KIITOS"},
-     {a : "KIITOS oli ensimmäinen sana"},
-     {a : "Kiitoksia sinulle"}]
-     console.log(testiA,"arrayA")
+    // Test array in finnish to play around with regEx
+    var arrayA = [
+        { a: "Kiitos kaikille avusta." },
+        { a: "Aina välillä tarvitaan kiitosta" },
+        { a: "Kuka sanoi kiitos sinulle metrossa?" },
+        { a: "Seuraava sana on KIITOS" },
+        { a: "KIITOS oli ensimmäinen sana" },
+        { a: "Kiitoksia sinulle" }]
 
-     var regDemo = testiA.map((item => {
-         if (kiitos.test(item.a)) {
-             var position = item.a.search(kiitos)
-             var whitespace = item.a.substr(position).search(blanko)
-            if (whitespace === -1) {whitespace= undefined}
-             var newString = item.a.substr(item.a.search(kiitos),whitespace)
-             console.log(item.a, "<-",kiitos +" regEx lause, sana löytyi indexillä",position, "seuraavaan whitespaceen asti teksti:",newString)
-             console.log(whitespace,"whitespace result")
-         } 
-     }))
+    // Goes through the array to uses regEx to each item
+    var regDemo = arrayA.map((item => {
+        // If item contains searchTerm = true
+        if (searchTerm.test(item.a)) {
+
+            // Position of index of the searchTerm in string
+            var position = item.a.search(searchTerm)
+
+            // Makes a substring starting from the index of position and searches position of the next whitespace
+            var whitespace = item.a.substr(position).search(blanko)
+
+            // If whitespace = -1 -> string ends so this must be undefined
+            if (whitespace === -1) { whitespace = undefined }
+
+            // Creates a new string by using positions of the found searchTerm and next whitespace (or undefined if it is the last word)
+            var newString = item.a.substr(item.a.search(searchTerm), whitespace)
+
+            // Own notes in Finnish
+            console.log(item.a, "<-", searchTerm + " regEx lause, sana löytyi indexillä", position, "seuraavaan whitespaceen asti teksti:", newString)
+            console.log(whitespace, "whitespace result")
+        }
+    }))
 
 
-     console.log(regDemo, "<- RegDemo funktion jälkeen")
+    console.log(regDemo, "<- RegDemo funktion jälkeen")
 
 
-        
+
 
 
 
