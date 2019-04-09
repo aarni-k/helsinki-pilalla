2d4737962fc5e7f4ca51279b554acff7dc243529
aarni-k <karjalainen.aarni@gmail.com>
2019-04-09 10:50:46 +0300

    Updates

--- precache-manifest.2f0c2b6933d241ba7742b82f2c2a62ec.js
+++ precache-manifest.2f0c2b6933d241ba7742b82f2c2a62ec.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/helsinki-pilalla/static/js/runtime~main.bfcdb760.js"
   },
   {
-    "revision": "03146534cf8cc91b8ef0",
-    "url": "/helsinki-pilalla/static/js/main.730a71a9.chunk.js"
+    "revision": "13aef222a375e85d3722",
+    "url": "/helsinki-pilalla/static/js/main.8c6cb842.chunk.js"
   },
   {
     "revision": "ad153bed86ec465fa433",
     "url": "/helsinki-pilalla/static/js/2.32d99623.chunk.js"
   },
   {
-    "revision": "03146534cf8cc91b8ef0",
-    "url": "/helsinki-pilalla/static/css/main.c8a64f0a.chunk.css"
+    "revision": "13aef222a375e85d3722",
+    "url": "/helsinki-pilalla/static/css/main.b3e15d12.chunk.css"
   },
   {
-    "revision": "51605de83511c47680945f6497ebeeff",
+    "revision": "baea9dddb74b600d30444e43ea971fde",
     "url": "/helsinki-pilalla/index.html"
   }
 ];
