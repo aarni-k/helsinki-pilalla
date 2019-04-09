afb2722add5913cb06dac524632b0467fd38a894
aarni-k <karjalainen.aarni@gmail.com>
2019-04-09 10:30:07 +0300

    another try

---
+++ precache-manifest.662010987e279f68606dca02d1118172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8f59034a05e0eaa622",
+    "url": "/helsinki-pilalla/static/js/runtime~main.bfcdb760.js"
+  },
+  {
+    "revision": "03146534cf8cc91b8ef0",
+    "url": "/helsinki-pilalla/static/js/main.730a71a9.chunk.js"
+  },
+  {
+    "revision": "ad153bed86ec465fa433",
+    "url": "/helsinki-pilalla/static/js/2.32d99623.chunk.js"
+  },
+  {
+    "revision": "03146534cf8cc91b8ef0",
+    "url": "/helsinki-pilalla/static/css/main.c8a64f0a.chunk.css"
+  },
+  {
+    "revision": "51605de83511c47680945f6497ebeeff",
+    "url": "/helsinki-pilalla/index.html"
+  }
+];
